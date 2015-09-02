a8f3eef916ae49d0da63fed05e709f19f7b7365d
Joe Carroll <josephcarroll1@gmail.com>
2015-09-01 20:53:45 -0400

    Fixed out by one error in averaging.

--- avg.js
+++ avg.js
@@ -1,20 +1,19 @@
 function Avg (sampleSize) {
     this.sampleSize = sampleSize;
-    this.samples = new Array(sampleSize);
-    this.sampleCount = 0;
+    this.samples = [];
 }
 
 Avg.prototype.sample = function(value) {
-	this.samples[this.sampleCount++] = value;
-	if (this.sampleCount >= this.sampleSize) {
+	this.samples.push(value);
+	if (this.samples.length >= this.sampleSize) {
 		this.samples = this.samples.slice(1, this.sampleSize);
 	}
 };
 
 Avg.prototype.get = function() {
 	var sum = 0;
-	for(var i = 0; i < this.sampleCount; i++) {
+	for(var i = 0; i < this.samples.length; i++) {
 		sum = sum + this.samples[i];
 	}
-	return sum / this.sampleCount;
+	return sum / this.samples.length;
 };
